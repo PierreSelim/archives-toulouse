0ed76ff561d20bbfbfefdf6ac15f1e348284e16e
Pierre-Selim <ps.huard@gmail.com>
2018-06-16 15:36:41 +0200

    JNA2018 Mapping description to wiki text

--- archivestls/collection.py
+++ archivestls/collection.py
@@ -1,12 +1,18 @@
+import configparser
 import logging
 import json
 import os
 import sys
+from collections import OrderedDict
 
+
+import mwclient
 from requests_html import HTMLSession, HTML
+from sickle import Sickle
 
 
 CACHE_PATH = '.cache_archivetls'
+USER_AGENT = 'Archives Toulouse Uploader'
 
 
 def metadata(url, html_session=None):
@@ -79,3 +85,102 @@ class CollectionBot(object):
         self.__log__.info('Writing metadata cache in %s', descr_cache)
         with open(descr_cache, 'w') as f:
             json.dump(descriptions, f, indent=2, ensure_ascii=False)
+
+
+class DublinCoreCollectionBot(object):
+
+    def __init__(self,
+                 name='archivetls',
+                 url=None,
+                 category=None):
+        self.__bot_name__ = name
+        self.__url_api__ = url
+
+        # logger
+        self.__log__ = logging.getLogger('archivetls')
+        self.__setup_logger__()
+
+        # cache
+        if not os.path.exists(CACHE_PATH):
+            os.mkdir(CACHE_PATH)
+        self.__cache_meta__ = os.path.join(CACHE_PATH, '{}_meta.json'.format(self.__bot_name__))
+        self.__cache_descr__ = os.path.join(CACHE_PATH, '{}_descr.json'.format(self.__bot_name__))
+
+        # mwclient
+        self.__site__ = mwclient.Site('commons.wikimedia.org',
+            clients_useragent=USER_AGENT)
+        config = configparser.RawConfigParser()
+        config.read(os.path.join(os.environ['HOME'], '.mwclient_login_rc'))
+        self.__site__.login(config.get('login', 'user'),
+                            config.get('login', 'pass'))
+
+        #category
+        self.__category__ = category
+
+    def __setup_logger__(self):
+        """Setting up the LOG."""
+        consolehandler = logging.StreamHandler(stream=sys.stdout)
+        formatter = logging.Formatter(
+            '%(asctime)s    %(levelname)s    %(message)s',
+            '%Y-%m-%d %H:%M:%S')
+        consolehandler.setFormatter(formatter)
+        consolehandler.setLevel(logging.INFO)
+        self.__log__.addHandler(consolehandler)
+        self.__log__.setLevel(logging.DEBUG)
+
+    def descriptions(self):
+        """Retrive descriptions and dumps it in cache file"""
+        s = Sickle(self.__url_api__)
+        records = [record for record in s.ListRecords(metadataPrefix='oai_dc')]
+        descr = OrderedDict()
+        subjects = OrderedDict()
+        cats = []
+        for record in records:
+            item = record.metadata['source'][0].split(',')[1].strip()
+            descr[item] = record.metadata
+            self.__log__.info('%s', item)
+            if 'subject' in record.metadata:
+                item_subjects = record.metadata['subject']
+                for t in item_subjects:
+                    if t in subjects:
+                        subjects[t] = subjects[t] + 1
+                    else:
+                        subjects[t] = 1
+                        cats.append(self.subject_to_category(t))
+            else:
+                self.__log__.warning('  no subject for %s', item)
+        self.__log__.info('Parsed %s items', len(records))
+        self.__log__.info('Subjects: %s', json.dumps(subjects, indent=2))
+        for cat in cats:
+            self.__log__.info('  [[%s]]', cat)
+            page = self.__site__.pages[cat]
+            if not page.exists:
+                page.save('[[{}]]'.format(self.__category__), 'Upload cat')
+        self.__log__.info('Dumping metadata %s', self.__cache_meta__)
+        with open(self.__cache_meta__, 'w') as f:
+            json.dump(descr, f, indent=4, ensure_ascii=False)
+
+    def subject_to_category(self, subject):
+        return '{} - {}'.format(self.__category__, subject)
+
+    def mapping(self):
+        """From descriptions generate mapping of descriptions and files."""
+        with open(self.__cache_meta__, 'r') as f:
+            items = json.load(f)
+            descr = {
+                item: self.item_mapping(item, items[item])
+                for item in items
+            }
+            with open(self.__cache_descr__, 'w') as cache:
+                self.__log__.info(
+                    'Dumping descriptions %s',
+                    self.__cache_descr__)
+                json.dump(descr, cache, indent=4, ensure_ascii=False)
+
+    def item_mapping(self):
+        return None
+
+    def run(self):
+        if not os.path.exists(self.__cache_meta__):
+            self.descriptions()
+        self.mapping()
